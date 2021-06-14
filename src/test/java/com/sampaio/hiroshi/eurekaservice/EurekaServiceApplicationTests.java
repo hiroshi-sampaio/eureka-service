8aa06cab231428d1089b1b2d6f4a7a866ba0ab39
Hiroshi Sampaio <17440771+hiroshi-sampaio@users.noreply.github.com>
2021-06-14 19:10:55 +0100

    1st

---
+++ src/test/java/com/sampaio/hiroshi/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampaio.hiroshi.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
